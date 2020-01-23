4fa66fb377b5e986e79a67deef3d9c28a8cb0d7f
PeterB <peterbui1995@gmail.com>
2020-01-23 12:09:47 +1100

    implemented randomised spawning

--- pages/molegame/mole.js
+++ pages/molegame/mole.js
@@ -20,6 +20,7 @@ const leavingMole = mole => {
 };
 const resetMole = mole => {
   mole.querySelector('#leaving').classList.remove('show');
+  mole.classList.remove('.active');
 };
 
 const noFeed = mole => {
@@ -33,10 +34,11 @@ const feed = mole => {
 };
 
 var starve;
+var canBeFed;
 
 const moleStarveCycle = mole => {
   hungryMole(mole);
-  starve = setTimeout(noFeed, 1000, mole);
+  starve = setTimeout(noFeed, 1500, mole);
 };
 
 const moleFeedCycle = mole => {
@@ -44,39 +46,78 @@ const moleFeedCycle = mole => {
   feed(mole);
 };
 
-/* Choose random holes to spawn a mole */
+/* Choose random holes to spawn a mole and spawn them periodically */
+const spawn = () => {
+  /* Detect click on 'hungry mole' */
 
-const chooseRandomHole = array => array[Math.floor(Math.random() * 2)];
+  const mole = chooseRandomHole(allHoles);
 
-const activateMole = chooseRandomHole(allHoles);
+  if (!mole.classList.contains('.active')) {
+    mole.classList.add('.active');
+    moleStarveCycle(mole);
 
-/* Detect click on 'hungry mole' */
+    const feedCycle = x => moleFeedCycle(mole);
 
-let mole = activateMole;
+    if (mole.querySelector('#hungry').classList.contains('show')) {
+      canBeFed = mole.addEventListener('click', feedCycle);
+      setTimeout(function() {
+        mole.removeEventListener('click', feedCycle);
+      }, 1400);
+    }
+  }
+};
 
-moleStarveCycle(mole);
+const spawnInterval = setInterval(spawn, Math.random() * 300 + 500);
 
-const feedCycle = x => moleFeedCycle(mole);
-
-if (mole.querySelector('#hungry').classList.contains('show')) {
-  mole.addEventListener('click', feedCycle);
-  setTimeout(function() {
-    mole.removeEventListener('click', feedCycle);
-  }, 1000);
-}
+const chooseRandomHole = array => array[Math.floor(Math.random() * 7)];
 
 /* Mouse image change to bird and worm*/
 const background = document.querySelector('.game');
 const birdCursor = document.querySelector('.bird');
 const wormCursor = document.querySelector('.worm');
-const hungryMole = document.querySelector('#hungry');
+const hungryMoles = document.querySelectorAll('#hungry');
 
-background.addEventListener('mousemove', e => {
-  birdCursor.style.left = `${e.clientX - 40}px`;
-  birdCursor.style.top = `${e.clientY - 40}px`;
+background.addEventListener('mouseenter', e => {
+  birdCursor.classList.add('show');
+
+  background.addEventListener('mousemove', e => {
+    birdCursor.style.left = `${e.clientX - 40}px`;
+    birdCursor.style.top = `${e.clientY - 40}px`;
+  });
 });
 
-wormCursor.addEventListener('movemove', e => {
-  wormCursor.style.left = `${e.clientX - 40}px`;
-  wormCursor.style.top = `${e.clientY - 40}px`;
+background.addEventListener('mouseleave', e => {
+  birdCursor.classList.remove('show');
 });
+
+/* Array.from(hungryMoles).forEach(m =>
+  m.addEventListener('mousemove', e => {
+    if (m.classList.contains('show')) {
+      wormCursor.style.left = `${e.clientX - 40}px`;
+      wormCursor.style.top = `${e.clientY - 40}px`;
+      birdCursor.classList.remove('show');
+      wormCursor.classList.add('show');
+    } else {
+      wormCursor.classList.remove('show');
+      birdCursor.classList.add('show');
+    }
+  })
+); */
+
+/*
+If hovering over a hungry show mole then change cursor 
+
+problem: 2 images are shown.
+
+we need to hide the general one when 2nd one shows.
+
+when 2nd one is 'done'
+- starve or feed cycle is procc'd
+- 1. after 1 second
+- 2. after a click happens before that 1 second
+
+Maybe move the z-index depending on which ones are showing?
+
+*/
+
+// fix issue where some aren't going inactive, and some images stack
